beb4e3a5882366c2f7abf80f6242a0429ac6ce42
drew28 <atreid@gmail.com>
2016-03-09 14:34:24 -0500

    Introducing httpurlconnection
    for service calls from the forms

---
+++ app/src/main/java/com/atreid/expresslanesimages/HttpResponse.java
@@ -0,0 +1,52 @@
+package package com.atreid.expresslanesimages;
+
+/**
+ * @author midris
+ */
+public class HttpResponse {
+    private int responseCode;
+    private String responseMessage;
+    private String responseBody;
+
+    public HttpResponse() {
+    }
+
+    public HttpResponse(int responseCode, String responseMessage, String responseBody) {
+        this.responseCode = responseCode;
+        this.responseMessage = responseMessage;
+        this.responseBody = responseBody;
+    }
+
+    public int getResponseCode() {
+        return responseCode;
+    }
+
+    public void setResponseCode(int responseCode) {
+        this.responseCode = responseCode;
+    }
+
+    public String getResponseMessage() {
+        return responseMessage;
+    }
+
+    public void setResponseMessage(String responseMessage) {
+        this.responseMessage = responseMessage;
+    }
+
+    public String getResponseBody() {
+        return responseBody;
+    }
+
+    public void setResponseBody(String responseBody) {
+        this.responseBody = responseBody;
+    }
+
+    @Override
+    public String toString() {
+        return "HttpResponse{" +
+               "responseCode=" + responseCode +
+               ", responseMessage='" + responseMessage + '\'' +
+               ", responseBody='" + responseBody + '\'' +
+               '}';
+    }
+}
